21da47077fd34463ffa2552ec77747fdda6b59af
venelinpetrov <venelin.petrov@hyperscience.com>
2020-04-23 14:08:07 +0300

    Add close btn

--- src/components/Notification/Notification.js
+++ src/components/Notification/Notification.js
@@ -3,6 +3,8 @@ import { EventBus, withInjector } from '../../services';
 
 const NotificationComponent = ({ injector }) => {
     const eventBus = injector.get(EventBus);
+    // Notice setNotification is used with its 'callback' form below. This is needed in order
+    // to get the notification from the previous state and append the new notification to them.
     const [notifications, setNotifications] = useState([]);
     const cb = notification => {
         setNotifications(notifications => notifications.concat(notification));
@@ -32,10 +34,22 @@ const NotificationComponent = ({ injector }) => {
 };
 
 const SingleNotification = ({ text, timeout=5000, onClose }) => {
-    setTimeout(() => {
+    // Auto-close
+    const timeoutId = setTimeout(() => {
         onClose();
     }, timeout);
-    return <div>{text}</div>;
+
+    // Manual close
+    const handleCloseBtn = () => {
+        clearTimeout(timeoutId);
+        onClose();
+    };
+    return (
+        <div>
+            {text}
+            <button onClick={handleCloseBtn}>X</button>
+        </div>
+    );
 };
 
 const Notification = withInjector(NotificationComponent, [EventBus]);
